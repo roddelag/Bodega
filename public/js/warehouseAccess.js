ec671beacdf24d748b2975875a7b29a3ad38722e
Emilio Rivers <rivers.emilio2001@protonmail.com>
2023-03-11 17:24:21 -0600

    Improved UI and added more functionality to app

--- public/js/warehouseAccess.js
+++ public/js/warehouseAccess.js
@@ -1,3 +1,32 @@
+let popup = document.getElementById("popup");
+let popupImg = document.getElementById("popup-img");
+let popupH1 = document.getElementById("popup-h1");
+
+var scanAllowed = true;
+
+// Function to display the popup with the directions of where to go
+function openPopup(orderStatus) {
+    scanAllowed = false;
+
+    // Adds the styling to the popup depending if the order is valid or not
+    if(orderStatus == "VALID") {
+        popupImg.src = "./assets/icons/valid-order-icon.png";
+        popupH1.innerHTML = "¡Código valido!"
+    } else {
+        popupImg.src = "./assets/icons/invalid-order-icon.png";
+        popupH1.innerHTML = "¡Código inválido!"
+    }
+    
+    popup.classList.add("active-popup");
+    setTimeout(closePopup, 5000);
+}
+
+// Function to hide the popup after a certain amount of time
+function closePopup() {
+    popup.classList.remove("active-popup");
+    scanAllowed = true;
+}
+
 // This method will trigger user permissions
 Html5Qrcode.getCameras().then(devices => {
     if (devices && devices.length) {
@@ -15,14 +44,18 @@ Html5Qrcode.getCameras().then(devices => {
                 }
             },
             (decodedText, decodedResult) => {
-                //  Verifies the ID of the order exctracted from the QR is valid
-                fetch('http://127.0.0.1:3000/pedido/' + decodedText)
-                    .then(res => res.json())
-                    .then((res) => {
-                        if(res.success) {
-                            console.log("SIUUUU");
-                        }
-                    });
+                if(scanAllowed) {
+                    //  Verifies the ID of the order from the QR is valid
+                    fetch('http://127.0.0.1:3000/pedido/' + decodedText)
+                        .then(res => res.json())
+                        .then((res) => {
+                            if(res.success) {
+                                openPopup("VALID");
+                            } else {
+                                openPopup("INVALID");
+                            }
+                        });
+                }
             },
             (errorMessage) => {
                 console.log("Error reading QR code...");
