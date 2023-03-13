11b69ad6a02e631c28fa6179816c7de0c90e6bc5
SilverGar <a00829450@itesm.mx>
2023-03-13 16:04:07 -0600

    hacer pedido terminado

--- public/js/make-order.js
+++ public/js/make-order.js
@@ -1,13 +1,54 @@
 function generateQR() {
     // Connect to the API that generates the QR, giving it
     // the text to encode within it
+    var downloadQr = document.querySelector('#qr-code');
+    var amount = document.querySelector('#qr-txt');
     let finalURL =
-            'https://chart.googleapis.com/chart?cht=qr&chl=' +
-            document.getElementById("qr-txt").value +
-            '&chs=160x160&chld=L|0'
-    
+        'https://chart.googleapis.com/chart?cht=qr&chl=' +
+        document.getElementById("qr-txt").value +
+        '&chs=160x160&chld=L|0'
+
     // Replace the src of the image with
     // the QR code image
     document.getElementById("qr-code").src = finalURL;
-    
+    downloadQr.setAttribute('href', finalURL);
+    downloadQr.style.display = 'inline-block';
+
+    fetch('http://127.0.0.1:3000/pedido', {
+        method: 'POST',
+        body: JSON.stringify({
+            "email": 'prueba@hotmail.com',
+            "cantidad": amount.value,
+            "precio": amount.value * 10,
+            "peso": 5,
+            "stats": "PENDING"
+        }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(results => results.json())
+        .then(function (data) {
+            console.log(data)
+        });
+
 }
+
+/*
+fetch('http://127.0.0.1:3000/pedido', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        "email": 'prueba@hotmail.com',
+                        "cantidad": amount.value,
+                        "precio": 10,
+                        "peso": 5
+                    }),
+                    headers: {
+                        "Content-Type": "application/json; charset=UTF-8"
+                    }
+                })
+                    .then(results => results.json())
+                    .then(function (data) {
+                        console.log(data)
+                    });
+*/
